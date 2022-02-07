8acb76788b4341059745ee25d6261196f4d203db
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 10:51:39 +0000

    update

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io('https://rimell.cc:5000')
+const socket = io.connect('https://rimell.cc:5000')
 // const socket = io('http://rimell.cc:5000')
 // const socket = io('http://localhost:5000')
 
