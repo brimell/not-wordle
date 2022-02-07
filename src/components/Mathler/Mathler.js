2356ab39ac01d9a8500a33b13a78d40e2777d733
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 14:15:53 +0000

    dura auto-backup

--- src/components/Mathler/Mathler.js
+++ src/components/Mathler/Mathler.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Mathler.css'
+import $ from 'jquery'
 
 export default function Mathler() {
+
+    $('body').css('background-color', '#f5f5f5')
 
     return (
         <div className="mathler">
