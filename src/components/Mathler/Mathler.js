e3b0a89a79616dd4253cd1b4907fbd0344555318
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 14:09:40 +0000

    dura auto-backup

--- src/components/Mathler/Mathler.js
+++ src/components/Mathler/Mathler.js
@@ -4,7 +4,7 @@ import $ from 'jquery'
 
 export default function Mathler() {
 
-    $("#iframe-id").contents().find("img").attr("style","width:100%;height:100%")
+    $("#mathlerIframe").contents().find("img").attr("style","width:100%;height:100%")
     return (
         <div className="mathler">
             <iframe id='mathlerIframe' title='mathler' className='mathlerIframe' src="https://www.mathler.com/" frameborder="0"></iframe>
