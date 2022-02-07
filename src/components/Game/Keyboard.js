b4771fb2fb63cad33343955af557a0a0c0e0dcfe
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 12:45:17 +0000

    dura auto-backup

--- src/components/Game/Keyboard.js
+++ src/components/Game/Keyboard.js
@@ -9,7 +9,7 @@ export function Keyboard(props) {
   ];
  
   return (
-    { hidden && <div className="Game-keyboard">
+    <div className="Game-keyboard" style={{ display: props.hidden && "none"}}>
       {keyboard.map((row, i) => (
         <div key={i} className="Game-keyboard-row">
           {row.map((label, j) => {
@@ -36,6 +36,6 @@ export function Keyboard(props) {
           })}
         </div>
       ))}
-    </div>}
+    </div>
   );
 }
