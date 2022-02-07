c82dba943d92de8c9d4235325cdb3848cb0e25d5
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 14:07:35 +0000

    dura auto-backup

--- src/components/Mathler/Mathler.js
+++ src/components/Mathler/Mathler.js
@@ -4,7 +4,7 @@ import $ from 'jquery'
 
 export default function Mathler() {
 
-    $('.mathler').style.border = '1px solid #000000'
+    // $('.mathler').style. = '1px solid #000000'
     return (
         <div className="mathler">
             <iframe title='mathler' className='mathlerIframe' src="https://www.mathler.com/" frameborder="0"></iframe>
