b24d8cb5de8f3c951aa4828e274f36ee86fab7ef
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 16:27:51 +0000

    dura auto-backup

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,16 +1,16 @@
 import { Clue, clueClass } from "../Game/clue";
-import './keyboard.css'
+import "./keyboard.css";
 
 export function Keyboard(props) {
-  const hidden = props.hidden
+  const hidden = props.hidden;
   const keyboard = [
     "q w e r t y u i o p".split(" "),
     "a s d f g h j k l".split(" "),
     "Enter z x c v b n m Backspace".split(" "),
   ];
- 
+
   return (
-    <div className="Game-keyboard" style={{ display: props.hidden && "none"}}>
+    <div className="Game-keyboard" style={{ display: props.hidden && "none" }}>
       {keyboard.map((row, i) => (
         <div key={i} className="Game-keyboard-row">
           {row.map((label, j) => {
