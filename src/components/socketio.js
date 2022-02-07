656b8333d3f6d9d34201020e2f7488a7ebafc672
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-07 11:21:33 +0000

    dura auto-backup

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -4,6 +4,4 @@ const socket = io.connect('https://rimell.cc:5000')
 // const socket = io('http://rimell.cc:5000')
 // const socket = io('http://localhost:5000')
 
-
-// this is a test
 export default socket
