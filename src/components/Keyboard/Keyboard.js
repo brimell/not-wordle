c7b14025cd7fa684748365a7c55f54b7ac5c2862
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-08 16:33:29 +0000

    dura auto-backup

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -11,6 +11,7 @@ export function Keyboard(props) {
   ];
 
   useEffect(() => {
+    console.log(loaded)
     window.addEventListener("load", () => {
       const btn = document.querySelectorAll(".Game-keyboard-button");
       console.log("btn: ", btn);
