dd8db52387dd84b546a42918ffe4d7a4e097133d
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-08 16:54:48 +0000

    dura auto-backup

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -137,6 +137,7 @@ function Game(props) {
     }
     if (guesses.length === props.maxGuesses) return;
     if (/^[a-z]$/.test(key)) {
+      console.log('now: ',currentGuess)
       setCurrentGuess((guess) => (guess + key).slice(0, wordLength));
       setHint("");
     } else if (key === "backspace") {
