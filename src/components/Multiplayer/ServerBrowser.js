ca88e55c9010ccf90e7ba060374126f911eb9b5e
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-08 13:07:12 +0000

    dura auto-backup

--- src/components/Multiplayer/ServerBrowser.js
+++ src/components/Multiplayer/ServerBrowser.js
@@ -24,7 +24,7 @@ export default function ServerBrowser(props) {
       )}
       {rooms.length === 0 &&
       <h2>no one is hosting a game right now</h2>
-        tempList.map((room, i) => {
+        {tempList.map((room, i) => {
           return (
             <article className="information card" key={i}>
               <div className="noRooms">
@@ -36,7 +36,7 @@ export default function ServerBrowser(props) {
               </div>
             </article>
           );
-        })}
+        })}}
     </div>
   );
 }
