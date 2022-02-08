8b6bd155d7e42084d01d62ac6e545faabd34070c
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-08 12:50:41 +0000

    dura auto-backup

--- src/components/Multiplayer/ServerBrowser.js
+++ src/components/Multiplayer/ServerBrowser.js
@@ -4,46 +4,59 @@ import Carousel from "../Carousel/Carousel";
 
 export default function ServerBrowser(props) {
   const rooms = props.rooms;
-  const socket = props.socket
-  const name = props.name
-  const setLobby = props.setLobby
+  const socket = props.socket;
+  const name = props.name;
+  const setLobby = props.setLobby;
   return (
     <div className="server-browser">
-      {rooms.length > 0 && <Carousel name={name} setLobby={setLobby} rooms={rooms} socket={socket} active={0} />}
-      {rooms.length === 0 && 
-        <article className="information card">
-        <span className="tag">{'    '}</span>
-        <h2 className="title">{'    '}</h2>
-        <span className="subtitle">Players List:</span>
-        {'    '}
-        <button className="button">
-          <span>Join Room</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 0 24 24"
-            width="24px"
-            fill="none"
-          >
-            <path d="M0 0h24v24H0V0z" fill="none" />
-            <path
-              d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z"
-              fill="currentColor"
-            />
-          </svg>
-        </button>
-        <dl className="details">
-          <div>
-            <dt>Players</dt>
-            <dd>{'    '}</dd>
-          </div>
-          <div>
-            <dt>State</dt>
-            <dd>{'    '}</dd>
-          </div>
-        </dl>
-      </article>
-      }
+      {rooms.length > 0 && (
+        <Carousel
+          name={name}
+          setLobby={setLobby}
+          rooms={rooms}
+          socket={socket}
+          active={0}
+        />
+      )}
+      {rooms.length === 0 && (
+        {for (var i = 0; i < 3; i++) {
+          return (
+            <article className="information card">
+          <span className="tag">{"    "}</span>
+          <h2 className="title">{"    "}</h2>
+          <span className="subtitle">Players List:</span>
+          {"    "}
+          <button className="button">
+            <span>Join Room</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="none"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path
+                d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+          <dl className="details">
+            <div>
+              <dt>Players</dt>
+              <dd>{"    "}</dd>
+            </div>
+            <div>
+              <dt>State</dt>
+              <dd>{"    "}</dd>
+            </div>
+          </dl>
+        </article>
+          )
+        }}
+        
+      )}
     </div>
   );
 }
