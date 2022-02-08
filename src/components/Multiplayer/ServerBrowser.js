8fe245a03cce4521752f7d64d060186ab0f0ca5e
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-08 13:00:30 +0000

    dura auto-backup

--- src/components/Multiplayer/ServerBrowser.js
+++ src/components/Multiplayer/ServerBrowser.js
@@ -26,7 +26,7 @@ export default function ServerBrowser(props) {
         tempList.map((room, i) => {
           return (
             <article className="information card" key={i}>
-              <span className="tag">{"    "}</span>
+              <span className="tag">{"       "}</span>
               <h2 className="title">{"    "}</h2>
               <button className="button">
                 <span>Join Room</span>
