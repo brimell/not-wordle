52e3877764c7054c563e82db0026fb522ee1f716
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-11 18:56:00 +0000

    added voice chat socket listeners to server

--- server/server.js
+++ server/server.js
@@ -196,6 +196,22 @@ io.on("connection", (socket) => {
     }
   });
 
+  // voice chat
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
   socket.on("disconnect", () => {
     let user = users.removeUser(socket.id);
 
