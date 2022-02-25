2e292b415fd5f6d042bf3d5f4cabf56f99f85e30
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-25 21:54:50 +0000

    dura auto-backup

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -1,4 +1,12 @@
 import io from "socket.io-client";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+function getPort() {
+    
+}
 
 const socket = io.connect('https://notwordle.herokuapp.com:5000')
 // const socket = io('https://rimell.cc:5000')
