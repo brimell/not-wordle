36492fd4d62ccae5dff8132f23e191664406af94
Raaydon <billjrimell@gmail.com>
2022-02-25 14:28:22 +0000

    update

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io.connect('https://not-wordle-server.herokuapp.com:5000/')
+const socket = io.connect('https://not-wordle-server.herokuapp.com:5000')
 // const socket = io('https://rimell.cc:5000')
 // const socket = io('http://localhost:5000')
 
