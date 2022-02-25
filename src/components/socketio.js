32aa8a08a99a9b0b417f486eb9f3b2c03b9ee971
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-25 21:55:11 +0000

    dura auto-backup

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -7,11 +7,14 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 const firestore = firebase.firestore();
 
 function getPort() {
-    
+	const messagesRef = firestore.collection("messages");
+	const query = messagesRef.orderBy("createdAt").limit(25);
+
+	const [messages] = useCollectionData(query, { idField: "id" });
 }
 
-const socket = io.connect('https://notwordle.herokuapp.com:5000')
+const socket = io.connect("https://notwordle.herokuapp.com:5000");
 // const socket = io('https://rimell.cc:5000')
 // const socket = io('http://localhost:3000')
 
-export default socket
+export default socket;
