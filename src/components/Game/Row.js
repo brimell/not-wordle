c4c9a5e8edef45453b1a83bbaf2c4436c81a5d66
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-25 22:03:18 +0000

    update

---
+++ src/components/Game/Row.js
@@ -0,0 +1,22 @@
+import { clueClass } from "./clue";
+
+export function Row(props) {
+	const isLockedIn = props.rowState === "LockedIn";
+	const letterDivs = props.cluedLetters
+		.concat(Array(props.wordLength).fill({ clue: "Absent", letter: "" }))
+		.slice(0, props.wordLength)
+		.map(({ clue, letter }, i) => {
+			let letterClass = "Row-letter";
+			if (isLockedIn && clue !== undefined) {
+				letterClass += " " + clueClass(clue);
+			}
+			return (
+				<div key={i} className={letterClass}>
+					{letter}
+				</div>
+			);
+		});
+	let rowClass = "Row";
+	if (isLockedIn) rowClass += " Row-locked-in";
+	return <div className={rowClass}>{letterDivs}</div>;
+}
