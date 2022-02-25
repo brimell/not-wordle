10705624dd0fb32e14efcc4f20ffcaf0c71f9c2c
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-25 22:01:40 +0000

    dura auto-backup

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -1,12 +1,13 @@
 import io from "socket.io-client";
-import axios from 'axios';
+import axios from "axios";
 
 function getPort() {
-    if (window.location.host.startsWith('localhost')) {
-        return 3000;
-    }
-    axios.get(window.location.origin + "/port")
-
+	if (window.location.host.startsWith("localhost")) {
+		return 3000;
+	}
+	axios.get(window.location.origin + "/port").then((res) => {
+		return res.data;
+	});
 }
 
 const socket = io.connect(`https://notwordle.herokuapp.com:${getPort()}`);
