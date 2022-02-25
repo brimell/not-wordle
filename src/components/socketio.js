08ea53a86dcdae6af53f85f3dda0078016e1bd67
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-25 21:56:41 +0000

    dura auto-backup

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -1,16 +1,8 @@
 import io from "socket.io-client";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-
-const firestore = firebase.firestore();
+import axios from 'axios';
 
 function getPort() {
-	const messagesRef = firestore.collection("messages");
-	const query = messagesRef.orderBy("createdAt").limit(25);
 
-	const [messages] = useCollectionData(query, { idField: "id" });
 }
 
 const socket = io.connect(`https://notwordle.herokuapp.com:${getPort()}`);
