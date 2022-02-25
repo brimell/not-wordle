9b36f82299a2f307ff62209b78bd42f5ea234aa4
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-25 21:53:15 +0000

    dura auto-backup

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -2,6 +2,6 @@ import io from "socket.io-client";
 
 // const socket = io.connect('https://notwordle.herokuapp.com:5000')
 // const socket = io('https://rimell.cc:5000')
-const socket = io('http://localhost:3000')
+// const socket = io('http://localhost:3000')
 
 export default socket
