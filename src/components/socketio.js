c6608f8aaecb8200b000f216207d4a1c2b860411
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-26 19:36:51 +0000

    test

--- src/components/socketio.js
+++ src/components/socketio.js
@@ -14,7 +14,8 @@ if (window.location.host.startsWith("localhost")) {
 } else if (window.location.host.startsWith("rimell")) {
 	socket = io.connect(`https://rimell.cc/notwordle`);
 } else {
-	socket = io.connect(`https://notwordle.herokuapp.com:${getPort()}`);
+	// socket = io.connect(`https://notwordle.herokuapp.com:${getPort()}`);
+	socket = io.connect(`https://notwordle.herokuapp.com`);
 }
 // const socket = io('https://rimell.cc:5000')
 // const socket = io('http://localhost:3000')
