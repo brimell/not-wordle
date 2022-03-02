d0b4b3f1acec0190115f14f596e3a333a309eeb2
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-03-02 17:26:47 +0000

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// publicDir: "./public",
-	// base: "/",
+	publicDir: "./public",
+	base: "/",
+  build: {
+    outDir: "build",
+    cssCodeSplit: true,
+    
+  }
 });
