4a354213ebd359b724e4e72b9b06f986d3e9b987
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-03-04 20:44:42 +0000

    removed animation

--- src/components/Game/Row.jsx
+++ src/components/Game/Row.jsx
@@ -1,42 +1,40 @@
-import { useRef, useEffect } from "react";
 import { clueClass } from "./clue";
 import { gsap } from "gsap";
 import $ from "jquery";
 export function Row(props) {
-	var refs_list = {}; // list of refs of each locked in letter
-
-	function lockInAnim(clue, letterClass) {
-		let tl = gsap.timeline();
-		if (refs_list) {
-			Object.keys(refs_list).forEach((i) => {
-				tl.to(refs_list[i].current, {
-					rotationX: "360",
-					duration: 1,
-					onComplete: () => {
-						letterClass += " " + clueClass(clue);
-					},
-				});
-			});
-		}
-		return letterClass;
-	}
+	// function lockInAnim(clue, letter) { // atempt at animation WIP
+	// 	let tl = gsap.timeline();
+	// 	if (refs_list) {
+	// 		Object.keys(refs_list).forEach((i) => {
+	// 			tl.to(refs_list[i].current, {
+	// 				rotationX: "360",
+	// 				duration: 1,
+	// 				onComplete: () => {
+	// 					console.log(letter);
+	// 					if (letter.current.parentElement.classList.contains("Row-locked-in")) {
+	// 						$("." + letter.current.className).addClass(
+	// 							clueClass(clue)
+	// 						);
+	// 					}
+	// 				},
+	// 			});
+	// 		});
+	// 	}
+	// }
 
 	const isLockedIn = props.rowState === "LockedIn";
 	const letterDivs = props.cluedLetters
 		.concat(Array(props.wordLength).fill({ clue: "absent", letter: "" }))
 		.slice(0, props.wordLength)
 		.map(({ clue, letter }, i) => {
-			refs_list[i] = useRef();
-
 			let letterClass = "Row-letter";
 
 			if (isLockedIn && clue !== undefined) {
-				var thisClass = refs_list[i].current.className
-				$('.' + thisClass) = lockInAnim(clue, letterClass);
+				letterClass += " " + clueClass(clue);
 			}
 
 			return (
-				<div key={i} ref={refs_list[i]} className={letterClass}>
+				<div key={i} className={letterClass}>
 					{letter}
 				</div>
 			);
