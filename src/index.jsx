7f42c7963bb7e18c07308007e4621f7d3549b5e7
Raaydon <billjrimell@gmail.com>
2022-03-15 12:45:27 +0000

    adding pwa with vite plugin

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-// import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 import { HashRouter as Router} from "react-router-dom";
 // import { BrowserRouter as Router} from "react-router-dom";
 
 //  uncomment to use custom right click
 // import './components/rc/rc'
+
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -19,3 +20,13 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
