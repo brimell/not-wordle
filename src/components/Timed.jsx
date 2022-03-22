6bd64a4d5821ea2fe0dafa14c10291cecb88a06b
Raaydon <billjrimell@gmail.com>
2022-03-22 11:17:58 +0000

    update timed

--- src/components/Timed.jsx
+++ src/components/Timed.jsx
@@ -5,36 +5,8 @@ import { useTimer } from "react-timer-hook";
 
 export default function Timed() {
 	const [currentGrid, setCurrentGrid] = useState([]);
-	const [timerState, setTimerState] = useState("stopped");
-	const [seconds, setSeconds] = useState(5);
+	const expiryTimestamp = Date.now() + 60000;
 
-	function handleGameFinish(gameState) {
-		return;
-	}
-
-	useEffect(() => {
-		if (currentGrid.length > 0) {
-			if (currentGrid[0].length > 0 && timerState === "stopped") {
-				setTimerState("started");
-			}
-		}
-		console.log(currentGrid);
-	}, [currentGrid]);
-
-	return (
-		<div className="GameContainer">
-			<Timer expiryTimestamp={Date.now() + 60000} />
-			,
-			<Game
-				target={false}
-				socket={false}
-				setCurrentGrid={setCurrentGrid}
-				handleGameFinish={handleGameFinish}
-			/>
-		</div>
-	);
-}
-function Timer({ expiryTimestamp }) {
 	const {
 		seconds,
 		minutes,
@@ -46,10 +18,14 @@ function Timer({ expiryTimestamp }) {
 		resume,
 		restart,
 	} = useTimer({
+		autoStart: false,
+		onExpire: timerFinish,
 		expiryTimestamp,
 		onExpire: () => console.warn("onExpire called"),
 	});
-
+	function timerFinish() {
+		// do stuff
+	}
 	function restartTimer() {
 		// restarts to 5 minute timer
 		const time = new Date();
@@ -57,14 +33,39 @@ function Timer({ expiryTimestamp }) {
 		restart(time);
 	}
 
+	function handleGameFinish(gameState) {
+		return;
+	}
+
+	useEffect(() => {
+		if (currentGrid.length > 0) {
+			if (currentGrid[0].length > 0 && !isRunning) {
+				start()
+			}
+		}
+		console.log(currentGrid);
+	}, [currentGrid]);
+
 	return (
-		<div style={{ textAlign: "center" }}>
-			<h1>react-timer-hook </h1>
-			<p>Timer Demo</p>
+		<div className="GameContainer">
+			<Timer minutes={minutes} seconds={seconds} />
+			<Game
+				target={false}
+				socket={false}
+				setCurrentGrid={setCurrentGrid}
+				handleGameFinish={handleGameFinish}
+			/>
+		</div>
+	);
+}
+function Timer(props) {
+	const { minutes, seconds } = props;
+
+	return (
+		<div className="timer">
 			<div>
 				<span>{minutes}</span>:<span>{seconds}</span>
 			</div>
-			<p>{isRunning ? "Running" : "Not running"}</p>
 		</div>
 	);
 }
